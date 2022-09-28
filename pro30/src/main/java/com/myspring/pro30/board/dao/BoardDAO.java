042df053266342d7bfc36443cf06fd5713ac1605
DESKTOP-64LP8AV\alstj <alstjr316412@naver.com>
2022-09-28 21:12:56 +0900

    Alsss7`s first commit

--- pro30/src/main/java/com/myspring/pro30/board/dao/BoardDAO.java
+++ pro30/src/main/java/com/myspring/pro30/board/dao/BoardDAO.java
@@ -1,14 +1,30 @@
 package com.myspring.pro30.board.dao;
 
 import java.util.List;
-import java.util.Map;
 
-import com.myspring.pro30.article.vo.ArticleVO;
+import com.myspring.pro30.board.vo.ArticleVO;
 
+/**
+ * 
+ * @version 1.0.0 2022년 9월 28일
+ * @author 김민석
+ *
+ */
 public interface BoardDAO {
 
-	public int insertNewArticle(Map articleMap);
+	/**
+	 * 
+	 * @return List
+	 * @throws Exception
+	 */
+	List selectAllArticlesList() throws Exception;
 
-	public List<ArticleVO> selectAllArticlesList();
+	/**
+	 * 
+	 * @param articleNO 글 번호
+	 * @return ArticleVO
+	 * @throws DataAccessException
+	 */
+	ArticleVO selectArticle(int articleNO) throws DataAccessException;
 
 }
--- pro30/src/main/java/com/myspring/pro30/board/service/BoardServiceImpl.java
+++ pro30/src/main/java/com/myspring/pro30/board/service/BoardServiceImpl.java
@@ -1,37 +1,37 @@
 package com.myspring.pro30.board.service;
 
 import java.util.List;
-import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
-import org.springframework.transaction.annotation.Transactional;
 
-import com.myspring.pro30.article.vo.ArticleVO;
 import com.myspring.pro30.board.dao.BoardDAO;
+import com.myspring.pro30.board.vo.ArticleVO;
 
+/**
+ * 
+ * @version 1.0.0 2022년 9월 28일
+ * @author 김민석
+ *
+ */
 @Service("boardService")
 @Transactional(propagation = Propagation.REQUIRED)
 public class BoardServiceImpl implements BoardService {
+	// 데이터베이스 접근 객체
 	@Autowired
 	private BoardDAO boardDAO;
 
-	/** 
-	 * ... DB에 저장된 모든 board 레코드를 가져온다.
-	 */
-	public List<ArticleVO> listArticles() throws Exception{
-		List<ArticleVO> articlesList =  boardDAO.selectAllArticlesList();
-        return articlesList;
+	// 글 목록을 불러옴
+	public List<ArticleVO> listArticles() throws Exception {
+		List<ArticleVO> articlesList = boardDAO.selectAllArticlesList();
+		return articlesList;
 	}
-	
-	/**
-	 *  ... 매개변수로 map을 받아 DB에 저장한다.
-	 * @param articleMap 저장될 레코드의 정보를 담고 있는 map
-	 * @return 가장 최근에 추가된 글 번호 반환
-	 */
+
+	// 하나의 글의 내용을 불러옴
 	@Override
-	public int addNewArticle(Map articleMap) throws Exception{
-		return boardDAO.insertNewArticle(articleMap);
+	public ArticleVO viewArticle(int articleNO) throws Exception {
+		ArticleVO articleVO = boardDAO.selectArticle(articleNO);
+		return articleVO;
 	}
 }
