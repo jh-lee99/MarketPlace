daa03e161610a1f2c4b695d1d1236a30cca4db50
winni305 <114224472+winni305@users.noreply.github.com>
2022-10-17 21:49:49 +0900

    Add files via upload

---
+++ src/main/java/com/PROJECT_ONE/main/MainController.java
@@ -0,0 +1,40 @@
+package com.bookshop01.main;
+
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.bookshop01.common.base.BaseController;
+import com.bookshop01.goods.service.GoodsService;
+import com.bookshop01.goods.vo.GoodsVO;
+
+@Controller("mainController")
+@EnableAspectJAutoProxy
+public class MainController extends BaseController {
+	@Autowired
+	private GoodsService goodsService;
+
+	@RequestMapping(value= "/main/main.do" ,method={RequestMethod.POST,RequestMethod.GET})
+	public ModelAndView main(HttpServletRequest request, HttpServletResponse response) throws Exception{
+		HttpSession session;
+		ModelAndView mav=new ModelAndView();
+		String viewName=(String)request.getAttribute("viewName");
+		mav.setViewName(viewName);
+		
+		session=request.getSession();
+		session.setAttribute("side_menu", "user");/*사이드 메뉴에 따라 옆 메뉴변경*/
+		Map<String,List<GoodsVO>> goodsMap=goodsService.listGoods();/*책 정보 조회후  map에 저장*/
+		mav.addObject("goodsMap", goodsMap);/*메인으로 상품 정보 전달*/
+		return mav;
+	}
+}
